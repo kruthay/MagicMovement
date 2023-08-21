32412937848d3c2d31501c1024de0b8ead7cb8be
Kruthay Donapati <117230425+kruthay@users.noreply.github.com>
2023-08-21 16:26:49 -0400

    Initial Commit

---
+++ MagicMovement/MagicMovementApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicMovementApp.swift
+//  MagicMovement
+//
+//  Created by Kruthay Donapati on 8/21/23.
+//
+
+import SwiftUI
+
+@main
+struct MagicMovementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
